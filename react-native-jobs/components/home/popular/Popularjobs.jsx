c1b45ffa78017a0c103daadfd4dce30dc893792c
lovhurt <alicandagidir@hotmail.com>
2024-08-16 17:19:34 +0300

    bug fix

--- react-native-jobs/components/home/popular/Popularjobs.jsx
+++ react-native-jobs/components/home/popular/Popularjobs.jsx
@@ -37,13 +37,9 @@ const Popularjobs = () => {
           <Text>Something went wrong</Text>
         ) : (
           <FlatList
-            data={[1, 2, 3, 4, 5, 6, 7, 8]}
+            data={data}
             renderItem={({ item }) => <PopularJobCard item={item} />}
-            keyExtractor={(item) => item
-              
-              // ?.job_id
-
-            }
+            keyExtractor={(item) => item?.job_id}
             contentContainerStyle={{ columnGap: SIZES.medium }}
             horizontal
           ></FlatList>
